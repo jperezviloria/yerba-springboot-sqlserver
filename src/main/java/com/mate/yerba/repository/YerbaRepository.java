13ed0b7adfc37b1f7edd95e3adbaf9c8c436b4b8
jperezviloria <protobot.dev@gmail.com>
2021-01-28 21:51:00 -0300

    creating queries with natives SQL

--- src/main/java/com/mate/yerba/repository/YerbaRepository.java
+++ src/main/java/com/mate/yerba/repository/YerbaRepository.java
@@ -2,8 +2,16 @@ package com.mate.yerba.repository;
 
 import com.mate.yerba.model.Yerba;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface YerbaRepository extends JpaRepository<Yerba, Integer> {
+
+    @Query(value = "SELECT * FROM yerba WHERE price_yerba = ?1", nativeQuery = true)
+    Yerba getYerbaByPrice(int priceYerba);
+
+    @Query(value = "SELECT * FROM yerba WHERE brand_yerba = ?1", nativeQuery = true)
+    Yerba getYerbaByBrand(String brandYerba);
+
 }
